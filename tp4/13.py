f0c54aa8a742f71130f5269dc9145e1bbf771fa7
Bruno Galvan <bruno.gab.galvan@gmail.com>
2024-04-10 02:08:58 -0300

    adding more

---
+++ tp4/13.py
@@ -0,0 +1,14 @@
+#Ejercicio 13: Hacer un programa que permita ingresar un número natural n. Informar si n es un término de la
+#serie de Fibonacci. En este caso indicar en que posición de la serie se encuentra.
+#Ejemplo: 1, 1, 2, 3, 5, 8 ... . Si el número ingresado es 4, el número no pertenece a la serie.
+#Si el número es 5, este pertenece a la serie y su orden es 5.
+#(Ley de formación de los términos en dicha serie: los dos primeros términos son 1 y cada uno
+#de los demás es igual a la suma de los dos anteriores).
+
+fibo_array = []
+value = int(input("Ingrese un valor: "))
+
+while True:
+    if value == 1:
+        print("El número pertenece a la secuencia de Fibonacci.")
+    elif value <
