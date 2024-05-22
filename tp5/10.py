b97c1fed5b9eadfed980b136c44f074b956ec10d
Bruno <bruno.gab.galvan@gmail.com>
2024-05-22 14:44:14 -0300

    tp10 finished

--- tp5/10.py
+++ tp5/10.py
@@ -3,4 +3,29 @@
 #programa mostrará mensajes indicando si el número ingresado es mayor o menor que el valor
 #secreto. Permitir que el usuario abandone al ingresar -1. Informar la cantidad de intentos realizada
 #al terminar el juego. Utilizar la función creada en el ejercicio 8 para resolver el problema.
+import random
+
+random_number = random.randint(1000, 9999)
+numero = int(input("Numero al azar generado! Adivinelo: "))
+
+def adivinador(number):
+    counter = 1
+    while number != random_number:
+        if number == -1:
+               return print("Termina el programa.")
+        if 1000 <= number and number <= 9999:
+            if number < random_number:
+                number = int(input("Mala suerte! Ingrese un valor mayor: "))
+            elif number > random_number:
+                number = int(input("Mala suerte! Ingrese un valor menor: "))
+            counter = counter + 1
+        else:
+            number = int(input("Ingrese un valor valido entre 1000 y 9999: "))
+
+    print("Ganaste! El valor era", number, "e intentaste", counter, "veces.")
+
+adivinador(numero)
+
+    
+
 
