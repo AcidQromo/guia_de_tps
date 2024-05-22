0407bc919dc626b4e109d80f59cb4d2b7e0379a5
Bruno <bruno.gab.galvan@gmail.com>
2024-05-22 01:03:34 -0300

    tp10

---
+++ tp5/10.py
@@ -0,0 +1,6 @@
+#Desarrollar un programa que genere un número entero al azar de cuatro cifras y proponerle al
+#usuario que lo descubra, ingresando valores repetidamente hasta hallarlo. En cada intento el
+#programa mostrará mensajes indicando si el número ingresado es mayor o menor que el valor
+#secreto. Permitir que el usuario abandone al ingresar -1. Informar la cantidad de intentos realizada
+#al terminar el juego. Utilizar la función creada en el ejercicio 8 para resolver el problema.
+
