db5754483f458c0a89415a90820f6b87d6a84441
Bruno <bruno.gab.galvan@gmail.com>
2024-06-16 17:36:10 -0300

    04 finished

--- tp7/04.py
+++ tp7/04.py
@@ -3,7 +3,45 @@
 #descendente?
 
 import modlists
-'''
+
+#Seleccion
+def ord_selection(lst):
+    aux = 0
+    for i in range(len(lst)):
+        min = i
+        for j in range(i + 1, len(lst)):
+            if lst[j] < lst[min]:
+                min = j
+
+        if min != i:
+            aux = lst[i]
+            lst[i] = lst[min]
+            lst[min] = aux
+
+    return lst
+
+#Insercion
+def ord_insertion(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i - 1
+
+        while j >= 0 and key < lst[j]:
+            lst[j + 1] = lst[j]
+            j = j - 1
+        lst[j + 1] = key
+
+    return lst
+
+#Burbujeo
+def ord_bubble(lst):
+    for _ in range(0, len(lst)):
+        for i in range(0, len(lst)):
+            if lst[i] > lst[i + 1]:
+                aux = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = aux
+
 desde = int(input("Ingrese desde que numero desea generar los valores (por ejemplo 1): "))
 hasta = int(input("Ingrese hasta que numero desea generar los valores (por ejemplo 20): "))
 
@@ -25,44 +63,15 @@ lista3 = modlists.generate_random_lst(desde, hasta, longitud)
 print("Lista3 generada con exito.")
 print(lista3)
 
-'''
-lista1 = [3,2,5,1,4]
-lista2 = [4,6,3,2,4]
-
-#Seleccion
-def ord_selection(lst):
-    
-    aux = 0
-    for i in range(len(lst)):
-        min_index = i
-        for j in range(i + 1, len(lst)):
-            if lst[j] < lst[min_index]:
-                min_index = j
-
-        if min_index != i:
-            aux = lst[i]
-            lst[i] = lst[min_index]
-            lst[min_index] = aux
-
-    return lst
-
-#Insercion
-def ord_insertion(lst):
-    for i in range(1, len(lst)):
-        key = lst[i]
-        j = i - 1
-
-        while j >= 0 and key < lst[j]:
-            lst[j + 1] = lst[j]
-            j = j - 1
-        lst[j + 1] = key
-
-    return lst
-        
-
-print(lista1)
+print("---")
+print("Lista1 ordenada usando seleccion:")
 print(ord_selection(lista1))
 
-print(lista2)
+print("---")
+print("Lista2 ordenada usando insercion:")
 print(ord_insertion(lista2))
+
+print("---")
+print("Lista3 ordenada usando burbujeo:")
+print(ord_insertion(lista3))
 
