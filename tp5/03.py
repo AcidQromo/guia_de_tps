2db1e4aedf70ab8d2fbca2e0ca78a832ed6919f0
Bruno <bruno.gab.galvan@gmail.com>
2024-06-16 22:52:36 -0300

    remake

--- tp5/03.py
+++ tp5/03.py
@@ -4,7 +4,33 @@
 #obtener_mes_en_texto(4) → devuelve “Abril”.
 
 def obtener_mes_en_texto(month):
-    ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"][month - 1]
+
+    if month == 1:
+        return "Enero"
+    elif month == 2:
+        return "Febrero"
+    elif month == 3:
+        return "Marzo"
+    elif month == 4:
+        return "Abril"
+    elif month == 5:
+        return "Mayo"
+    elif month == 6:
+        return "Junio"
+    elif month == 7:
+        return "Julio"
+    elif month == 8:
+        return "Agosto"
+    elif month == 9:
+        return "Septiembre"
+    elif month == 10:
+        return "Octubre"
+    elif month == 11:
+        return "Noviembre"
+    elif month == 12:
+        return "Diciembre"
+    else:
+        return " "
 
 mes = int(input("Ingrese el numero de mes entre 1 y 12: "))
 
